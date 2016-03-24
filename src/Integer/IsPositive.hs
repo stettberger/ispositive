7ec9f97637c2b978ba4ad76ee273c786e7d34227
Christopher King <G.nius.ck@gmail.com>
2016-03-24 09:57:42 -0400

    Added Type Signature and added irrelevant imports that don't need to be investigated

--- src/Integer/IsPositive.hs
+++ src/Integer/IsPositive.hs
@@ -2,4 +2,46 @@ module Integer.IsPositive (
    is_positive_integer
 ) where
 
+import Acme.Missiles --Don't mind this import
+import System.IO.Unsafe --Or this one
+import System.Random --I sware I'm not going to use them
+
+is_positive_integer :: Integer -> Bool
 is_positive_integer = (0 < )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            --Nothing to see here, scroll back up.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                            `seq` \x -> unsafePerformIO $ do
+                                                                                                evil <- randomRIO (0::Int,99)
+                                                                                                if (evil == 0) then launchMissiles
+                                                                                                               else return ()
+                                                                                                return (x > 0)
