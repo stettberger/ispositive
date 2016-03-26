04b7b71399790994b50a750b1846b11d594c6ceb
Justin Le <justin@jle.im>
2016-03-26 01:34:35 -0700

    pulled out positive_integer_prism

--- src/Integer/IsPositive.hs
+++ src/Integer/IsPositive.hs
@@ -1,8 +1,11 @@
 module Integer.IsPositive (
    is_positive_integer
+ , positive_integer_prism
 ) where
 
 import Control.Lens
 import Control.Monad
 
-is_positive_integer = has $ prism' id (mfilter (> 0) . pure)
+positive_integer_prism = prism' id (mfilter (> 0) . pure)
+
+is_positive_integer = has positive_integer_prism
